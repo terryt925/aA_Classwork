4845f012cc34a4521ced92c12ce8de8ab4b0d6aa
terryt925 <thindterry@yahoo.com>
2021-02-05 12:44:24 -0800

    Make phase 01

--- W3D5/skeleton 2/lib/00_tree_node.rb
+++ W3D5/skeleton 2/lib/00_tree_node.rb
@@ -1,3 +1,38 @@
 class PolyTreeNode
+  def initialize(value)
+    @parent = nil
+    @children = []
+    @value = value
+  end
+
+  def parent
+    @parent
+  end
+
+  def parent=(new_parent)
+    @parent.children.delete(self) if @parent != nil
+    if new_parent == nil
+      @parent = nil
+    else
+      @parent = new_parent
+      @parent.children << self if !@parent.children.include?(self)
+    end
+  end
+
+  def children
+    @children
+  end
+
+  def value
+    @value
+  end
+
+  def add_child(child)  # old_child.add_child(child)  cur_node.add_child(new_node)
+    child.parent = self
+  end
+
+  def remove_child
+
+  end
 
 end
