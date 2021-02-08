49d6b4b7850ccdc41c54cef85bdbcf5effcd6e80
“thomasgill16” <thomasgill16@gmail.com>
2021-02-08 16:23:15 -0600

    Fix everyting with build_move_tree

--- W4D1/skeleton/lib/knights_travails.rb
+++ W4D1/skeleton/lib/knights_travails.rb
@@ -3,26 +3,37 @@ require_relative "00_tree_node.rb"
 
 class KnightPathFinder
 
-  attr_reader :root_node
+  attr_reader :root_node, :considered_positions
 
   def self.valid_moves(pos)
-    output = []
+    x, y = pos
+    output = [[x + 2, y + 1],
+              [x + 2, y - 1],
+              [x - 2, y + 1],
+              [x - 2, y - 1],
+              [x + 1, y + 2],
+              [x - 1, y + 2],
+              [x + 1, y - 2],
+              [x - 1, y - 2]]
+    output.select do |pos|
+      pos[0] > -1 && pos[0] < 8 && pos[1] > -1 && pos[1] < 8
+    end
     
   end
 
   def initialize(start_pos)
     # @pos = start_pos
-    @root_node = start_pos
+    @root_node = PolyTreeNode.new(start_pos)
     @considered_positions = [start_pos]
   end
 
   def new_move_Positions(pos)
     arr = KnightPathFinder.valid_moves(pos)
     output = []
-    arr.each do |node|
-      if !@considered_positions.include?(node)
-        @considered_positions << node
-        output << node
+    arr.each do |position|
+      if !@considered_positions.include?(position)
+        @considered_positions << position
+        output << position
       end
     end
     return output
@@ -38,10 +49,12 @@ class KnightPathFinder
     queue = [@root_node]
     while !queue.empty?
       node = queue.shift
-      return node if node.value == target
-      queue += new_move_Positions(node)
+      new_move_Positions(node.value).each do |pos|
+        child_node = PolyTreeNode.new(pos)
+        queue << child_node
+        node.add_child(child_node)
+      end
     end
-    nil
   end
   
 
