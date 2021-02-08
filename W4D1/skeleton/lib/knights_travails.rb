f742f3e653f2302ed9b36894ffa10d804e8734bd
“thomasgill16” <thomasgill16@gmail.com>
2021-02-08 14:08:20 -0600

    add valid_moves and new_move_Positions methods

--- W4D1/skeleton/lib/knights_travails.rb
+++ W4D1/skeleton/lib/knights_travails.rb
@@ -5,13 +5,22 @@ class KnightPathFinder
 
   attr_reader :root_node
 
-  def initialize(start_pos)
-    @pos = start_pos
-    @root_node = nil
+  def self.valid_moves(pos)
+
   end
 
-  def find_path
-    puts "hello"
+  def initialize(start_pos)
+    # @pos = start_pos
+    @root_node = start_pos
+    @considered_positions = [start_pos]
+  end
+
+  def new_move_Positions(pos)
+    if !@considered_positions.include?(pos)
+      KnightPathFinder.valid_moves(pos)
+      @considered_positions << pos
+      return pos
+    end
   end
 
 end
