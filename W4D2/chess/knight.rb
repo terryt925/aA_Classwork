dfc45ef334f9861995517c31c7e4c5d6e3b19c75
terryt925 <thindterry@yahoo.com>
2021-02-10 12:17:25 -0800

    Work on pawn

--- W4D2/chess/knight.rb
+++ W4D2/chess/knight.rb
@@ -3,7 +3,24 @@ class Knight < Piece
 
 include Stepable
 
+def symbol
 
+end
 
+def move_diffs
+  x,y = self.position
+
+  all_moves = [
+    [x-2, y-1],
+    [x-2, y+1],
+    [x-1, y+2],
+    [x-1, y-2],
+    [x+1, y+2],
+    [x+1, y-2],
+    [x+2, y-1],
+    [x+2, y+1]
+  ]
+
+end
 
 end
--- W4D2/chess/pawn.rb
+++ W4D2/chess/pawn.rb
@@ -1,10 +1,19 @@
 require_relative "piece"
 class Pawn < Piece
 
+def symbol
 
+end
 
+def moves
 
+end
 
+private
+
+def @_start_row?
+  
+end
 
 
 end
