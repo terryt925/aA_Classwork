3fe8a2263dded308d88244f4d314d32685712658
lpoole <keithpoole123@gmail.com>
2021-02-09 20:06:44 -0500

    add piece classes

--- W4D2/chess/slideable.rb
+++ W4D2/chess/slideable.rb
@@ -1,6 +1,22 @@
 module Slideable 
+HORIZONTAL_DIRS = [
+  [1,0],
+  [-1,0],
+  [0,1],
+  [0,-1]
+]
 
-def move_dir
+DIAGONAL_DIRS = [
+  [1,1],
+  [1,-1],
+  [-1,1],
+  [-1,-1]
+]
+def move_dir(value)
+  return HORIZONTAL_DIRS if value == horizontal
+  return DIAGONAL_DIRS if value == diagonal
+  return HORIZONTAL_DIRS + DIAGONAL_DIRS
+end
 
 private
 
