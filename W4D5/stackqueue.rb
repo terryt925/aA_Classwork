8808763768447f91026971564fbe20a6c3f8606b
aladdin731 <aladdin731@hotmail.com>
2021-02-12 17:02:01 -0800

    stackqueue WIP

--- W4D5/stackqueue.rb
+++ W4D5/stackqueue.rb
@@ -3,7 +3,30 @@ require_relative "mystack"
 
 class StackQueue
 
+  def initialize
+    @store_1 = MyStack.new
+    @store_2 = MyStack.new 
+  end
 
+  def size 
+    @store_1.size + @store_2.size 
+  end
+
+  def empty?
+    size == 0
+  end
+
+  def enqueue(item)
+    @store_1.push(item)
+  end
+
+  def dequeue
+    return @store_2.pop unless @store_2.empty?
+    until @store_1.empty?
+      @store_2.push(@store_1.pop)
+    end
+
+  end
 
 
 end
