3dbcdf3a9349d7f80262e6f49e354f641b04a307
terryt925 <thindterry@yahoo.com>
2021-02-15 13:52:54 -0800

    Create init methods

--- W5D1/hash_map_lru_cache/lib/p01_int_set.rb
+++ W5D1/hash_map_lru_cache/lib/p01_int_set.rb
@@ -1,19 +1,30 @@
 class MaxIntSet
+
+attr_reader :store
+
   def initialize(max)
+    @store = Array.new(max, false) 
   end
 
   def insert(num)
+    raise "Out of bounds" unless is_valid?(num)
+    @store[num] = true
   end
 
   def remove(num)
+    raise "Out of bounds" unless is_valid?(num)
+    @store[num] = false
   end
 
   def include?(num)
+    raise "Out of bounds" unless is_valid?(num)
+    @store[num]
   end
 
   private
 
   def is_valid?(num)
+    num < @store.length && num >= 0
   end
 
   def validate!(num)
