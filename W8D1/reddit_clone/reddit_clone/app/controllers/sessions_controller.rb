9ac6c24b873011dd32e46c2c83d78dfc7ff9630d
Sou Lee <Sou_Lee92@yahoo.com>
2021-03-08 10:15:53 -0800

    Complete sessions controller and application controller

---
+++ W8D1/reddit_clone/reddit_clone/app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  def new
+    render :new
+  end
+
+  def create
+    @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
+
+    if @user
+      login!(@user)
+      redirect_to users_url
+    else
+      flash.now[:errors] = @user.errors.full_messages
+      render :new
+    end
+
+  end
+
+  def destroy
+    logout!
+    redirect_to new_session_url
+  end
+end
