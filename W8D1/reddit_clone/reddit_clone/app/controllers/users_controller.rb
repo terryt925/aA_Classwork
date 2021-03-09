a651241312468e5fccc1176bc1af8375b014b0b4
terryt925 <thindterry@yahoo.com>
2021-03-08 17:15:31 -0800

    Complete phase 1

--- W8D1/reddit_clone/reddit_clone/app/controllers/users_controller.rb
+++ W8D1/reddit_clone/reddit_clone/app/controllers/users_controller.rb
@@ -1,2 +1,44 @@
 class UsersController < ApplicationController
+
+  before_action :require_logged_in, only: [:index, :show]
+
+  def index
+    @users = User.all
+
+    render :index
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+
+    render :show
+  end
+
+  def new
+    @user = User.new
+
+    render :new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      login!(@user)
+      redirect_to users_url
+    else
+      flash.now[:errors] = @user.errors.full_messages
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+
 end
+
+
+
