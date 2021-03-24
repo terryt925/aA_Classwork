fe3d7a8f59219430820326683a4a96608a8c0a99
mike <michaeldeanmdmph@gmail.com>
2021-03-23 18:00:09 -0700

    clock, css

---
+++ W10D2/widgets/frontend/clock.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+class Clock extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            time: new Date()
+        }
+        this.tick = this.tick.bind(this)
+        // this.render = this.render.bind(this)
+    }
+    tick() {
+        this.setState({ time: new Date() })
+        
+    }
+    
+    componentDidMount(){
+      this.timerId = setInterval(this.tick, 1000)   
+      //this has to be () =>  this.tick(), 1000
+      // or setInterval( this.tick, 1000)  ...dont envoke callback
+    }  
+
+    componentWillUnmount() {
+        clearInterval(this.timerId);
+    }
+
+    render(){
+
+        let hours = this.state.time.getHours();
+        let minutes = this.state.time.getMinutes(); 
+        let seconds = this.state.time.getSeconds();
+
+        return(
+            <div> 
+                <h1 className='clock'>Clock</h1>
+                <p className='time'>{hours}:{minutes}:{seconds}</p>
+            </div>             
+        )
+            
+        
+    }
+
+
+}
+
+export default Clock;
--- W10D2/widgets/frontend/widgets.jsx
+++ W10D2/widgets/frontend/widgets.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Clock from './clock'
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   const hello = <h1>Hello World</h1>;
 
-  ReactDOM.render(hello,root);
+  ReactDOM.render(<Clock/>,root);
 })
