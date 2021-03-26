37dc216d35288efc3c9c2ec5baff07757fc6171a
Chris Hanma <hongchris96@hotmail.com>
2021-03-26 15:41:33 -0700

    Completes up to beginning of Phase 4

--- W10D4/todo/frontend/components/todos/todo_list_container.jsx
+++ W10D4/todo/frontend/components/todos/todo_list_container.jsx
@@ -1,6 +1,8 @@
 import { connect } from 'react-redux';
 import TodoList from './todo_list';
 import { receiveTodo } from '../../actions/todo_actions';
+import { removeTodo } from '../../actions/todo_actions';
+
 import { allTodos } from '../../reducers/selectors';
 
 
@@ -11,7 +13,8 @@ const mapStateToProps = (state) => {
 }
 
 const mapDispatchToProps = dispatch => ({
-  receiveTodo: todo => dispatch(receiveTodo(todo))
+  receiveTodo: todo => dispatch(receiveTodo(todo)),
+  removeTodo: todo => dispatch(removeTodo(todo))
 });
 
 export default connect(
