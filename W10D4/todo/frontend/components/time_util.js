537cf13200b6f5c18825a4d855090ecd52e1600a
Chris Hanma <hongchris96@hotmail.com>
2021-03-29 15:20:05 -0700

    Remove extra stuff

--- W10D4/todo/frontend/components/time_util.js
+++
@@ -1,5 +0,0 @@
-
-
-export const uniqueId = () => {
-  return new Date().getTime();
-}
--- W10D4/todo/frontend/reducers/selectors.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export const allTodos = (state) => {
-  return Object.values(state.todos);
-}
